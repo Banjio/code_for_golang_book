d5f2f173d635b9db8ad77b11f43a70041346001e
Maximilian Beier <beiersmaxi@gmail.com>
2022-03-29 23:22:45 +0200

    First chapter completed

---
+++ hello_world/hello.go
@@ -0,0 +1,11 @@
+// Declare the package as main
+package main
+
+// Functions for formatting text from standard library, including printing
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+	//fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
+}
